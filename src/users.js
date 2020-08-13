0c7e04790cedf6b5a24b919ad4a1f3f282040399
liza p <liyourlp@gmail.com>
2020-08-12 20:41:16 -0700

    created users arr

---
+++ src/users.js
@@ -0,0 +1,11 @@
+//define users arr
+const users = [
+  {firstName: "Steve", lastName: "Smith", role: "Software Architect", salary: 150000},
+  {firstName: "Anna", lastName: "Lee", role: "UX designer", salary: 140000},
+  {firstName: "Elisabeth", lastName: "Goldman", role: "Engineering Manager", salary: 180000},
+  {firstName: "Elsa", lastName: "Winterbottom", role: "Security Engineer", salary: 135000},
+  {firstName: "Wyatt", lastName: "Oleff", role: "Software Engineer", salary: 130000},
+  {firstName: "Zack", lastName: "Gillmore", role: "Software Engineer", salary: 135000}
+]
+
+module.exports=users;
