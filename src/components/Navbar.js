ce14c6e035d96cb179c0aa8e435fc3b65c04f4b2
liza p <liyourlp@gmail.com>
2020-08-12 20:56:06 -0700

    bootstrap navbar temp

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar(){
+  return(
+   <div>
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+    </nav>
+
+    </div>
+  )
+}
+export default Navbar;
