2b78fd4b6ea148e56a36547636bddf5d9a4a87a8
liza p <liyourlp@gmail.com>
2020-08-13 20:48:36 -0700

    Updates

---
+++ precache-manifest.963088a91d469e37316f97ab2658d635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911d022899205d75073a44ad344a3c22",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "0e3ca67e0560957dc29c",
+    "url": "/react-employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ea44b54483d6e628fd2",
+    "url": "/react-employee-directory/static/js/2.5e0aec15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.5e0aec15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3ca67e0560957dc29c",
+    "url": "/react-employee-directory/static/js/main.8059278d.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  }
+]);
