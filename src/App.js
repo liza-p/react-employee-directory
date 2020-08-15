a375949dcb31904c2dc6aa0511095adc900e1134
liza p <liyourlp@gmail.com>
2020-08-14 17:11:57 -0700

    filter function

--- src/App.js
+++ src/App.js
@@ -8,48 +8,72 @@ import users from "./users.json";
 function App() {
   const [sortCol, setSortCol] = useState("");
   const [sortDir, setSortDir] = useState("asc");
-  const onSort = function(col){
+  const [search, setSearch] = useState("");
+
+  const onSort = function (col) {
     setSortCol(col);
-    if(sortDir === "asc"){
+    if (sortDir === "asc") {
       setSortDir("desc")
-    }else{
+    } else {
       setSortDir("asc")
     }
-  
-
+    
   }
 
-  if(sortDir ==="asc"){
-    users.sort(function(u1, u2){
+  let displayedUsers = users.filter(function(user){
+    if(user.firstName.toLowerCase().includes(search.toLowerCase())){
+      return true;
+    }
+    if(user.lastName.toLowerCase().includes(search.toLowerCase())){
+      return true;
+    }
+    if(user.role.toLowerCase().includes(search.toLowerCase())){
+      return true;
+    }
+    return false;
+  
+  });
+
+  if (sortDir === "asc") {
+    displayedUsers.sort(function (u1, u2) {
       if (u1[sortCol] > u2[sortCol]) {
         return 1;
       }
       if (u1[sortCol] < u2[sortCol]) {
-          return -1;
+        return -1;
       }
       return 0;
     })
-  }else{
-    users.sort(function(u1, u2){
+  } else {
+    displayedUsers.sort(function (u1, u2) {
       if (u1[sortCol] > u2[sortCol]) {
         return -1;
       }
       if (u1[sortCol] < u2[sortCol]) {
-          return 1;
+        return 1;
       }
       return 0;
     })
   }
 
+  const handleInputChange = function (event) {
+    console.log(event.target);
+    setSearch(event.target.value);
+  };
+
   return (
     <div className="App">
-     <Navbar/>
-     <div className="container">
-     <Search/>
-     <Table users={users} onSort={onSort}/>
-     </div>
+      <Navbar />
+      <div className="container">
+        <Search onChange = {handleInputChange}/>
+        <Table users={displayedUsers} onSort={onSort} />
+      </div>
     </div>
   );
+
+
 }
+
+
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Search(){
-  return(
+function Search(props) {
 
-  <form class="form-inline mt-3 mb-3">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
-
-
-
+  return (
+    <form class="form-inline mt-3 mb-3">
+      <input 
+        class="form-control mr-sm-2" 
+        type="text" placeholder="Search" 
+        aria-label="Search" name="search" 
+        onChange={props.onChange} 
+        // value={props.search} 
+      />
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
   )
 }
 export default Search;
